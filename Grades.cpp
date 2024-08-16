09ea50c6f135b3df472967b26d492307024b21f8
CWoods8032 <159725881+CWoods8032@users.noreply.github.com>
2024-08-16 18:34:30 -0500

    Add project files.

---
+++ Grades.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Function prototypes
+void applyCurve(double* gradePtr, double curvePercentage);
+void displayFinalGrade(double grade);
+
+int main() {
+	// Welcome message
+	cout << "Welcome to the Grade Curving Program!" << endl;
+	cout << "This program will curve a student's grade by a certain percentage." << endl << endl;
+
+}
